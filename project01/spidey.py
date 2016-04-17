bf8fc7a16b9778cfbb354e08bfade8d231bc543b
mchen6 <mchen6@nd.edu>
2016-04-17 19:46:54 -0400

    Not done with spidey.py

--- project01/spidey.py
+++ project01/spidey.py
@@ -92,25 +92,132 @@ class BaseHandler(object):
 #HTTPHandler
 
 class HTTPHandler(BaseHandler):
-    def handle(self): #need to overwrite handle
+    def __init__(self, fd, address, docroot = None):
+        BaseHandler.__init__(self, fd, address)
+        self.docroot = DOCROOT
 
-        # read lines
-        data = self.stream.readline().rstrip()
-
-        #done when there is an empty line
-        while data:
-            sys.stdou.write(data)
+    def _parse_request(self):
+        try:
+            # read lines
             data = self.stream.readline().rstrip()
+            request = data.split(' ')
+            REQUEST_METHOD = request[0]
+            REQUEST_URI = request[1]
+            parse_request_uri = REQUEST_URI.split('/')
+            QUERY_STRING = ''########INCOMPLETE 
+            #done when there is an empty line
+            while data:
+                self.debug('Read {}', data)
+                #sys.stdout.write(data)
+                data = self.stream.readline().rstrip()
+                temp = data.split(':')
+                if temp[0] == 'Host':
+                    os.environ['HTTP_HOST']=temp[1]
+                    os.environ['HTTP_REFERER']='http://' + temp[1] + parse_request_uri[1]
+                elif temp[0] == 'Connection':
+                    os.environ['HTTP_CONNECTION']=temp[1]
+                elif temp[0] == 'Accept':
+                    os.environ['HTTP_ACCEPT']=temp[1]
+                elif temp[0] == 'Upgrade-Insecure-Requests':
+                    os.environ['HTTP_UPGRADE_INSECURE_REQUESTS']=temp[1]
+                elif temp[0] == 'User-Agent':
+                    os.environ['HTTP_USER_AGENT']=temp[1]
+                elif temp[0] == 'Accept-Encoding':
+                    os.environ['HTTP_ACCEPT_ENCODING']=temp[1]
+                elif temp[0] == 'Accept-Language':
+                    os.environ['HTTP_ACCEPT_LANGUAGE']=temp[1]
+                
+        except socket.error:
+            pass #ignore socket errors
 
-        self.stream.write('HTTP/1.0 200 OK\r\n')
-        self.stream.write('Content-Type: text/html\r\n')
-        self.stream.write('\r\n')
+
+    def startDoc(self, uripath):
+        #check if the uripath starts with docroot
+        #split both uripath and docroot into lists
+        docrootlist=self.docroot.split('/')
+        uripathlist = uripath.split('/')
+        #check each element to compare
+        for i in range(len(docrootlist)):
+            if docrootlist[i] != uripathlist[i]:
+                #if fails, then return false
+                return False
+        #if reaches end, return true
+        return True
+
+    def _handle_error(self, number):
+        if number == 404:
+            pass 
+            # Display 404 error page
+        if number == 403: 
+            pass 
+            # Display 403 error page
+
+    def _handle_script(self):
+        #NEED to do
+        pass
+    def _handle_file(self):
+        # Determine the file's mimetype 
+        mimetype, _ = mimetypes.guess_type(self.uripath)
+        if mimetype is None:
+            mimetype = 'application/octet-stream'
+
+        # Write the http response status to socket
+        
+
+            self.stream.write('{}\r\n'.format(response))
+            self.stream.write('Content-Type: {}\r\n'.format(mimetype))
+            self.stream.write('\r\n')
+            urifilef = open(self.urifile,'rb')
+            try:
+                byte=urifilef.read(1)
+                while byte!= "":
+                    self.stream.write(byte)
+                    byte=f.read(1)
+            except:
+                pass
+            finally:
+                urifilef.close()
+
+    def _handle_directory(self):
+        #write HTML code
+        pass
+    
+
+
+
+    def handle(self): #need to overwrite handle from BasHandler
+        self.debug('Handle')
+        
+        #Pars HTTP request and headers
+        self._parse_request()
+
+        #Build uripath by normalizing REQUEST_URI
+        self.uripath = os.path.normpath(self.docroot + os.environ['REQUEST_URI'])
+
+        if not self.uripath or not self.startDoc(self.uripath):
+            self._handle_error(404) #404 error
+            #Need to write func
+        
+        elif os.path.isfile(self.uripath) and os.access(self.uripath, os.X_OK):
+            self._handle_script() #CGI script 
+            ##need to write func
+
+        elif os.path.isfile(self.uripath) and os.access(self.uripath, os.R_OK):
+            self._handle_file() #Static file
+            #need to write func
+
+        elif os.path.isdir(self.uripath) and os.acess(self.uripath, os.R_OK):
+            self._handle_directory() #Directory listing
+            #need to write func
+
+        else:
+            self._handle_error(403) #403 error
 
 # EchoHandler Class
-
+'''
 class EchoHandler(BaseHandler):
     def handle(self):
-        ''' Handle connection by reading data and then writing it back until EOF '''
+        # Handle connection by reading data and then writing it back until EOF 
         self.debug('Handle')
 
         try:
@@ -122,12 +229,12 @@ class EchoHandler(BaseHandler):
                 data = self.stream.readline()
         except socket.error:
             pass    # Ignore socket errors
-
+'''
 # TCPServer Class
 
 class TCPServer(object):
 
-    def __init__(self, address=ADDRESS, port=PORT, handler=EchoHandler):
+    def __init__(self, address=ADDRESS, port=PORT, handler=HTTPHandler):
         ''' Construct TCPServer object with the specified address, port, and
         handler '''
         self.logger  = logging.getLogger()                              # Grab logging instance
