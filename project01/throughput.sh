9cae8bad4cd86ff8cc146e15db37bb0bf8868097
Andrew D Paek <apaek1@student00.cse.nd.edu>
2016-04-20 22:31:54 -0400

    THE END

--- project01/throughput.sh
+++ project01/throughput.sh
@@ -20,7 +20,12 @@ done
 #100 trials
 TOTAL=0
 TIME=0
-
+for kilo in $(seq 1); do
+  ./thor.py -p 10 -r 10 -v http://student00.cse.nd.edu:$PORT/www/throughput/kilobyte 2>&1 > /dev/null | grep "Average" | awk -F" " '{print $8}' >> throughput_kilo_test2.txt
+done
+for mega in $(seq 1); do
+  ./thor.py -p 10 -r 10 -v http://student00.cse.nd.edu:$PORT/www/throughput/megabyte 2>&1 > /dev/null | grep "Average" | awk -F" " '{print $8}' >> throughput_mega_test2.txt
+done
 for giga in $(seq 1); do
   ./thor.py -p 1 -r 1 -v http://student00.cse.nd.edu:$PORT/www/throughput/gigabyte 2>&1 > /dev/null | grep "Average" | awk -F" " '{print $8}' >> throughput_giga_test2.txt
 done
