a40b70c1f71c1bb2691d9aec22f9f395376dd7bd
mchen6 <mchen6@nd.edu>
2016-04-24 14:35:35 -0400

    fury.py

--- project02/fury.py
+++ project02/fury.py
@@ -2,38 +2,94 @@
 
 import sys
 import work_queue
+import string
+import json
+import os
 
 #Constants
 
-LENGTH = int(sys.argv[1])
-ATTEMPTS = int(sys.argv[2])
-HASHES = sys.argv[3]
-TASKS = int(sys.argv[4])
-SOURCES = ('mini-hulk.py', HASHES)
+SOURCES = ('hulk.py', 'hashes.txt')
 PORT = 9123
-
-
+ALPHABET = string.ascii_lowercase + string.digits
 # Main Execution
 
 if __name__ == '__main__':
-	# Create a master and the task list
-	queue = work_queue.WorkQueue(PORT, name = 'fury-samuel', catalog=True)
-	queue.specify_log('mini-fury.log')
+	# Create a master
+	  # 1. Random port between 9000 - 9999
+	  # 2. Project name of fury-samuel
+	  # 3. Catalog mode enabled
+	queue = work_queue.WorkQueue(PORT, name = 'fury-mchen6apaek1', catalog=True)
+	queue.specify_log('fury.log') #Specify Work Queue log location
 
-	for _ in range(TASKS):
-		command = './mini-hulk.py {} {} {}'.format(LENGTH, ATTEMPTS, HASHES)
+	# Create task list
+
+	# Check passwords for length of <6 
+	for num in range(5):
+		command = './hulk.py -l {}'.format(num)
 		task = work_queue.Task(command)
 
+		# Add source files (transfer files to workers)
 		for source in SOURCES: 
 			task.specify_file(source, source, work_queue.WORK_QUEUE_INPUT)
+		# Submit tasks
 		queue.submit(task) 
 
+	# Checking passwords of length 6
+	for prefix in (ALPHABET):
+		command = './hulk.py -l 5 -p {}'.format(prefix) 
+		task = work_queue.Task(command)
+
+		# Add source files (transfer files to workers)
+		for source in SOURCES: 
+			task.specify_file(source, source, work_queue.WORK_QUEUE_INPUT)
+		# Submit tasks
+		queue.submit(task) 
+
+	#length 7
+	for prefix in itertools.product(ALPHABET,repeat=2):
+		command = './hulk.py -l 5 -p {}'.format(prefix) 
+		task = work_queue.Task(command)
+
+		# Add source files (transfer files to workers)
+		for source in SOURCES: 
+			task.specify_file(source, source, work_queue.WORK_QUEUE_INPUT)
+		# Submit tasks
+		queue.submit(task) 
+
+	# Checking passwords of length 8
+	for prefix in itertools.product(ALPHABET,repeat=3):
+		command = './hulk.py -l 5 -p {}'.format(prefix) 
+		task = work_queue.Task(command)
+
+		# Add source files (transfer files to workers)
+		for source in SOURCES: 
+			task.specify_file(source, source, work_queue.WORK_QUEUE_INPUT)
+		# Submit tasks
+		queue.submit(task) 
+
+
+	JOURNAL={}
+	# Until the Master queue 
 	while not queue.empty():
-		task = queue.wait()
+		task = queue.wait() # wait on task
+		
 		if task and task.return_status == 0: #check if task is valid 
 			sys.stdout.write(task.output) #output of workers should be passwords
+			JOURNAL[task.command] = task.output.split()
+			if command in JOURNAL:
+				print >>sys.stderr, 'Already did',command
+			else:
 
+				with open('journal.json.new','w') as stream:
+					json.dump(JOURNAL, stream)
+				os.rename('journal.json.new','journal.json')
+
+
+#-----------------------------------------------------------------
 # source env.sh
-# run mini-fury.py
+# ./mini-fury.py
+
 # work queue does not start the worker; you must do a manual worker
-	# work_queue_worker -d all -N fury-samuel 
+	# work_queue_worker -d all -N fury-mchen6apaek1
+
+# condor _submit_workers -N fury-mchen6apaek1 200 # 200 friends
