86aefca4afe25fbeb73713cacf156e4cac814110
mchen6 <mchen6@nd.edu>
2016-04-24 16:30:25 -0400

    env.sh

---
+++ project02/env.sh
@@ -0,0 +1,7 @@
+#!/bin/csh 
+
+setenv PATH /afs/nd.edu/user37/condor/software/sbin:$PATH
+setenv PATH /afs/nd.edu/user37/condor/software/bin:$PATH
+setenv PATH /afs/nd.edu/user37/ccl/software/cctools/bin:$PATH
+setenv PYTHONPATH /afs/nd.edu/user37/ccl/software/cctools/lib/python2.6/site-packages
+
